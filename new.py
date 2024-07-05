c54f5347bcda1af612c8020b3d6db3903287e905
Shaurya508 <shaurya@arymalabs.com>
2024-07-05 15:12:36 +0530

    Update new.py

--- new.py
+++ new.py
@@ -29,16 +29,16 @@ from PIL import Image
 import io
 # Load the Google API key from the .env file
 # load_dotenv()
-# API_KEY = "AIzaSyCvw_aGHyJtLxpZ4Ojy8EyaEDtPOzZM29"
+API_KEY = "AIzaSyCvw_aGHyJtLxpZ4Ojy8EyaEDtPOzZM29"
 
 # Configure Google Generative AI API key
-# genai.configure(api_key=API_KEY)
+genai.configure(api_key=API_KEY)
 
 # genai.configure(api_key=os.getenv('GOOGLE_API_KEY'))
 # Load the Google API key from the .env file
-load_dotenv()
+# load_dotenv()
 # genai.configure(api_key=st.secrets["GOOGLE_API_KEY"])
-genai.configure(api_key=os.getenv('GOOGLE_API_KEY'))
+# genai.configure(api_key=os.getenv('GOOGLE_API_KEY'))
 
 # sec_key=os.getenv("HUGGINGFACEHUB_API_TOKEN")
 # os.environ["HUGGINGFACEHUB_API_TOKEN"]=sec_key
