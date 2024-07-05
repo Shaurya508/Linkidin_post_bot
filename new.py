4f76cfd21bd963c760ca9fbe9dc1629242ee286c
Shaurya508 <shaurya@arymalabs.com>
2024-07-05 12:48:14 +0530

    Update new.py

--- new.py
+++ new.py
@@ -30,10 +30,10 @@ import io
 # Load the Google API key from the .env file
 # load_dotenv()
 # genai.configure(api_key=st.secrets["GOOGLE_API_KEY"])
-genai.configure(api_key=os.getenv('GOOGLE_API_KEY'))
+# genai.configure(api_key=os.getenv('GOOGLE_API_KEY'))
 # Load the Google API key from the .env file
 # load_dotenv()
-# genai.configure(api_key=st.secrets["GOOGLE_API_KEY"])
+genai.configure(api_key=st.secrets["GOOGLE_API_KEY"])
 # genai.configure(api_key=os.getenv('GOOGLE_API_KEY'))
 
 # sec_key=os.getenv("HUGGINGFACEHUB_API_TOKEN")
