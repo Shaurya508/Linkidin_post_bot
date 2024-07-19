0f7d01837b9dd7ea7f919bd22edc2541380ba7b0
Shaurya508 <shaurya@arymalabs.com>
2024-07-19 17:43:55 +0530

    Update new.py

--- new.py
+++ new.py
@@ -29,6 +29,8 @@ import io
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import NoSuchElementException, StaleElementReferenceException
+import re
 # Load the Google API key from the .env file
 # load_dotenv()
 # API_KEY = "AIzaSyCvw_aGHyJtLxpZ4Ojy8EyaEDtPOzZM29"
@@ -67,65 +69,23 @@ def linkedin_login(email, password , driver):
 def scrape_linkedin_post(url, driver):
     # Open the LinkedIn post URL
     driver.get(url)
-    
+    try:
+        img_elements = driver.find_elements(By.TAG_NAME, 'img')
+
+    # Extract and print image URLs
+        image_url = [img.get_attribute('src') for img in img_elements]
+        # image_url = image_element.get_attribute('src')
+    except:
+        image_url = None
     # Wait for the content to load
     try:
         post_content = WebDriverWait(driver, 10).until(
             EC.presence_of_element_located((By.CLASS_NAME, 'feed-shared-update-v2__description'))
         )
-        return post_content.text.encode('ascii', 'ignore').decode('ascii')
+        return post_content.text.encode('ascii', 'ignore').decode('ascii') , image_url
     except:
-        return "Could not find the main content of the post."
+        return "Could not find the main content of the post." , image_url
 
-
-
-def extract_text_from_url(url):
-    response = requests.get(url)
-    soup = BeautifulSoup(response.text, 'html.parser')
-    article_content = soup.find_all('p')
-    text = '\n'.join([p.get_text() for p in article_content])
-    return text.encode('ascii', 'ignore').decode('ascii')
-
-def extract_text_from_pdf(pdf_path):
-    # Convert PDF to images
-    pages = convert_from_path(pdf_path, 300)
-
-    # Iterate through all the pages and extract text
-    extracted_text = ''
-    for page_number, page_data in enumerate(pages):
-        # Perform OCR on the image
-        text = pytesseract.image_to_string(page_data)
-        extracted_text += f"Page {page_number + 1}:\n{text}\n"
-    return extracted_text
-#     with open(pdf_path, "rb") as pdfFile:
-#         pdfReader = PdfReader(pdfFile)
-#         numPages = len(pdfReader.pages)
-#         all_text = ""
-#         for page_num in range(numPages):
-#             page = pdfReader.pages[page_num]
-#             text = page.extract_text()
-#             if text:
-#                 all_text += text.encode('ascii', 'ignore').decode('ascii') + "\n"
-#     return all_text
-
-def extract_code_from_github(raw_url):
-    # text = extract_text_from_url('https://github.com/facebookexperimental/Robyn/blob/main/demo/demo.R')
-    # print(text)
-    # URL of the raw content of the R script on GitHub
-    # raw_url = "https://raw.githubusercontent.com/facebookexperimental/Robyn/main/demo/demo.R"
-
-    # Send a GET request to the raw content URL
-    response = requests.get(raw_url)
-
-    # Check if the request was successful
-    if response.status_code == 200:
-        # Get the content of the response
-        code = response.text
-
-        # Print the scraped code
-        return code
-    # else:
-    #     print(f"Failed to retrieve the URL. Status code: {response.status_code}")
 
 def get_text_chunks(text):
     text_splitter = RecursiveCharacterTextSplitter(chunk_size=2500, chunk_overlap=500)
@@ -141,113 +101,42 @@ def get_vector_store(text_chunks, batch_size=100):
         text_embeddings.extend(zip(batch, batch_embeddings))
     
     vector_store = FAISS.from_embeddings(text_embeddings, embedding=embeddings)
-    vector_store.save_local("faiss_index3")
+    vector_store.save_local("faiss_index_Images")
     return vector_store
 
+class Document:
+    def __init__(self, page_content):
+        self.page_content = page_content
 
 # Initialize an empty list to store conversation history
 conversation_history = []
 
 def user_input(user_question):
-    # global conversation_history  # Access the global conversation history list
-    
-    # # Initialize the model and embeddings
-    # model = ChatGoogleGenerativeAI(model="gemini-pro", temperature=0.5)
-    
-    # embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
-    
-    # # Load the FAISS vector store
-    # vector_db = FAISS.load_local("faiss_index1", embeddings, allow_dangerous_deserialization=True)
-    
-    # # Initialize the retriever with the vector store
-    # retriever = MultiQueryRetriever.from_llm(retriever=vector_db.as_retriever(k=8), llm=model)
-    # docs = retriever.get_relevant_documents(query=user_question)
-    # Retrieve relevant documents based on the current question only
-    # input_documents = retriever.get_relevant_documents(query=user_question)
-    
-#     prompt_template = """ You are a chatbot named MMM GPT developed by ARYMA LABS having a conversation with a human.Try to understand the context, chat history and question properly and then give detailed answers as much as possible. Don't answer if answer is not from the context however do talk like a human in a well mannered way.
-#     provide every answer in detailed explanation and easy words to make easy for the User.Give more weightage to question.
-#     when the question is asking for code:
-#     provide Short part of code Write that "Code looks like this " before giving code in a good format and easy to copy format.after providing the code , give Github Link from the paragraph from which you give the code in the end like :
-#     "Please check detailed code at" : Github link
-#     when question is not for code :
-#     but always provide link!
-#     Always provide one URL link or linkidin blog link given in the context in the following way in the end of the Answer whenver you think it's correct to provide link.
-#     "For more details visit" : URL link or linkidin link \n\n
-
-#     chat history : {chat_history}
-#     context : {context}
-#     question: {human_input}
-# Chatbot:"""
     prompt_template = """
-    Answer the question as detailed as possible from the provided context, make sure to provide all the details.\n
-    Your answer should look like it is taken from the context only , don't tell any thing from outside the context.\n
-    Also provide Linkedin Link from the context.
-    "For more details visit" : Linkedin Link \n\n
+    Explain the LinkedIn post in the context in as much detail as possible.
     Context:\n{context}?\n
-    Question:\n{question} + Explain in detail .\n
+    Question:\n{question}.\n
     Answer:
     """
-#     PROMPT = PromptTemplate(
-#         template=prompt_template, input_variables=["context" , "question"]
-#     )
-#     # chain_type_kwargs = { "prompt" : PROMPT }
 
-#     memory = ConversationBufferMemory(memory_key='chat_history',input_key="human_input" , return_messages=True)
-#     # conversation_chain = ConversationalRetrievalChain.from_llm(
-#     #     llm = model,
-#     #     retriever= vector_db.as_retriever(k=6),
-#     #     memory = memory,
-#     #     combine_docs_chain_kwargs=chain_type_kwargs
-#     # )
-#     chain = load_qa_chain(
-#     model, chain_type="stuff", memory=memory, prompt=PROMPT
-# )
-#     return chain({"input_documents": docs, "human_input": user_question}, return_only_outputs=True) , docs
     model = ChatGoogleGenerativeAI(model="gemini-pro", temperature=0)
-    # model_name = "Intel/dynamic_tinybert"
-
-# Load the tokenizer associated with the specified model
-    # tokenizer = AutoTokenizer.from_pretrained(model_name, padding=True, truncation=True, max_length=512)
-    # Define a question-answering pipeline using the model and tokenizer
-    # question_answerer = pipeline(
-    # "question-answering", 
-    # model=model_name, 
-    # tokenizer=tokenizer,
-    # return_tensors='pt'
-# )
-
-# Create an instance of the HuggingFacePipeline, which wraps the question-answering pipeline
-# with additional model-specific arguments (temperature and max_length)
-    # model = HuggingFacePipeline(
-    # pipeline=question_answerer,
-    # model_kwargs={"temperature": 0.7, "max_length": 512},
-# )
-    # repo_id="mistralai/Mistral-7B-Instruct-v0.2"
-    # model=HuggingFaceEndpoint(repo_id=repo_id,max_length=128,temperature=0.7,token=sec_key)
     prompt = PromptTemplate(template=prompt_template, input_variables=["context", "question"])
     chain = load_qa_chain(model, chain_type="stuff", prompt=prompt)
     embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")  # Model for creating vector embeddings
-    new_db = FAISS.load_local("faiss_index", embeddings, allow_dangerous_deserialization=True)  # Load the previously saved vector db
-    
-
-    # chain , model = get_conversational_chain()
+    new_db = FAISS.load_local("faiss_index_Images", embeddings, allow_dangerous_deserialization=True)  # Load the previously saved vector db
 
     mq_retriever = MultiQueryRetriever.from_llm(retriever = new_db.as_retriever(search_kwargs={'k': 1}) , llm =  model)
-
-
     docs = mq_retriever.get_relevant_documents(query=user_question)
-    # docs = new_db.similarity_search(query=user_question, k = 2)
- 
+    # Regular expression to find the last URL
+    page_content = docs[0].page_content
+    # Find all URLs in the page_content
+    urls = re.findall(r'https?://\S+', page_content)
 
-
-    # docs = new_db.similarity_search(query=user_question, k=10)  # Get similar text from the database with the query
+    # Get the last URL from the list
+    image_address = urls[-1] if urls else None
+    post_link = urls[0]
     response = chain({"input_documents": docs, "question": user_question}, return_only_outputs=True)
-    return response, docs
-
-    # response  = conversation_chain({"question": user_question})
-    # return(response.get("answer"))
-
+    return response, image_address , post_link
 
 def extract_links(pdf_path):
     links = []
@@ -264,15 +153,43 @@ def extract_links(pdf_path):
         # If OCR is needed
         pix = page.get_pixmap()
         img = Image.open(io.BytesIO(pix.tobytes()))
-        
-        # Extract links from OCR text (optional, if links are in image form)
-        # You may need to use regex to find links in the OCR text
 
     return links
 
 
 
+from selenium.common.exceptions import StaleElementReferenceException
 
+# Function to extract the URL of the next image in the sequence
+def extract_next_image_url(post_url,driver):
+    driver.get(post_url)
+    WebDriverWait(driver, 10).until(
+        EC.presence_of_all_elements_located((By.TAG_NAME, 'img'))
+    )
+    
+    try:
+        # Update the selector to match the LinkedIn post container
+        post_container = driver.find_element(By.CSS_SELECTOR, 'div.feed-shared-update-v2')  # Update the selector here
+        
+        # Get all image elements within the post container
+        img_elements = post_container.find_elements(By.TAG_NAME, 'img')
+        
+        if len(img_elements) < 2:
+            print("Not enough images found")
+            return None
+        
+        # Identify the current image (for example, the first one in the list)
+        current_img = img_elements[0]  # Update index if needed
+        current_img_src = current_img.get_attribute('src')
+        
+        # Find the next image in the sequence
+        next_img = img_elements[1]  # Adjust index as needed
+        next_img_src = next_img.get_attribute('src')
+        
+        return next_img_src
+    except NoSuchElementException:
+        print("Element not found")
+        return None
 
 def load_in_db():
     url_text_chunks = []
@@ -280,7 +197,7 @@ def load_in_db():
     # Set up the WebDriver (make sure chromedriver is in your PATH or provide the path to the executable)
     driver = webdriver.Chrome()
 
-    linkedin_email = "shaurya@arymalabs.com"
+    linkedin_email = "shauryamishra120210@gmail.com"
     linkedin_password = "Mishra@123"
     # Log in to LinkedIn
     linkedin_login(linkedin_email, linkedin_password , driver)
@@ -289,11 +206,12 @@ def load_in_db():
     # file_path = 'Linkidin_blogs.xlsx'
     # df = pd.read_excel(file_path, header=None)
     for linkedin_post_url in links:
-        post_text = scrape_linkedin_post(linkedin_post_url , driver)
+        post_text,_ = scrape_linkedin_post(linkedin_post_url , driver)
         text_chunks = get_text_chunks(post_text)
+        image_address = extract_next_image_url(linkedin_post_url , driver)
         for chunk in text_chunks:
-            url_text_chunks.append(f"Linkedin Link : {linkedin_post_url}\n{chunk}")
-
+            url_text_chunks.append(f"Linkedin Link : {linkedin_post_url}\n{chunk}\n{image_address}")
+    
     get_vector_store(url_text_chunks)
 
 def main():
